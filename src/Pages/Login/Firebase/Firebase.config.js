68252c69721369135702fecd95f6a0bb20d27bdd
shakil-1318 <shakiljust21@gmail.com>
2021-10-19 23:53:34 +0600

    fini

--- src/Pages/Login/Firebase/Firebase.config.js
+++ src/Pages/Login/Firebase/Firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDAnCnyfl5huam7P57PPP0_LpJdVkYoTLM",
-    authDomain: "medics-health-care.firebaseapp.com",
-    projectId: "medics-health-care",
-    storageBucket: "medics-health-care.appspot.com",
-    messagingSenderId: "495617873410",
-    appId: "1:495617873410:web:e2e0a1bf7233f836a6791b"
+    apiKey: "AIzaSyAULNrr0OgCu52ywFVrj9351IqEHTk7q2c",
+    authDomain: "medics-health-care-be077.firebaseapp.com",
+    projectId: "medics-health-care-be077",
+    storageBucket: "medics-health-care-be077.appspot.com",
+    messagingSenderId: "582652219050",
+    appId: "1:582652219050:web:d644e0478f666e84918ad3"
 };
 export default firebaseConfig;
